6728a6862c339ead6905d929d3d552518c3fa43c
Enrique Mireles <enriquemg96@gmail.com>
2020-08-05 17:02:45 -0500

    use obs audio as input for google meet

--- .scripts/init-session.sh
+++ .scripts/init-session.sh
@@ -9,3 +9,6 @@ xset fp rehash
 
 # Set wallpapers as background.
 feh --randomize --bg-fill $HOME/.config/i3/wallpapers/*
+
+# Create virtual1 sink for OBS
+pactl load-module module-null-sink sink_name="OBS-Monitor" sink_properties=device.description="OBS-Monitor"
